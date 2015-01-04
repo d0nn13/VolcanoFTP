598056243512b787c3a9390bbe265f642a02cf44
Samir Ahamada <ahamad_s@etna-alternance.net>
2015-01-04 20:50:22 +0100

    Minor change

--- lib/volcano_ftp/command/cwd.rb
+++ lib/volcano_ftp/command/cwd.rb
@@ -16,7 +16,7 @@ class FTPCommandCwd < FTPCommand
       session.set_cwd(path)
       FTPResponse.new(250, "Directory changed to #{path}")
 
-    rescue FTP530; FTPResponse.new(530, "Ya ain't logged.")
+    rescue FTP530; FTPResponse.new(530, 'Ya ain\'t logged.')
     rescue FTP550; FTPResponse.new(550, 'CWD command failed (directory does not exist)')
     rescue => e
       puts self.class, e.class, e, e.backtrace
